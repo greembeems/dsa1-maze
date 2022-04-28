5a78b6761175704b2e8b9b1c06f2a2a538277102
NashFarrand <Nrf3093@rit.edu>
2022-04-28 19:59:36 -0400

    Added Extern "C"

--- psychic-octo-spork/psychic-octo-spork/mazefunctions.cpp
+++ psychic-octo-spork/psychic-octo-spork/mazefunctions.cpp
@@ -25,13 +25,13 @@ int length = 0;
 Graph* aGraph;
 
 // Returns string with team members' names
-__declspec(dllexport) char* GetTeam()
+extern "C" __declspec(dllexport) char* GetTeam()
 {
     return (char*)team;
 }
 
 // Sets maze data from main program
-__declspec(dllexport) bool SetMaze(const int** data, int width, int height)
+extern "C" __declspec(dllexport) bool SetMaze(const int** data, int width, int height)
 {
     if (width >= 1 || height >= 1) {
         mData = data;
@@ -44,7 +44,7 @@ __declspec(dllexport) bool SetMaze(const int** data, int width, int height)
 }
 
 // Return maze data that was passed in from SetMaze
-__declspec(dllexport) int** GetMaze(int& width, int& height)
+extern "C" __declspec(dllexport) int** GetMaze(int& width, int& height)
 {
     width = mWidth;
     height = mHeight;
@@ -57,7 +57,7 @@ __declspec(dllexport) int** GetMaze(int& width, int& height)
 }
 
 // Returns next x and y position
-__declspec(dllexport) bool GetNextPosition(int& xpos, int& ypos)
+extern "C" __declspec(dllexport) bool GetNextPosition(int& xpos, int& ypos)
 {
     // Calls A Star for first instance only
     if (currentIndex == 0)
@@ -85,7 +85,7 @@ __declspec(dllexport) bool GetNextPosition(int& xpos, int& ypos)
 
 // Sets starting location for player
 // Save x and y value
-__declspec(dllexport) bool SetStart(int xpos, int ypos)
+extern "C" __declspec(dllexport) bool SetStart(int xpos, int ypos)
 {
     sXPos = xpos;
     sYPos = ypos;
@@ -99,7 +99,7 @@ __declspec(dllexport) bool SetStart(int xpos, int ypos)
 
 // Get starting location for player
 // Return x and y, -1 if not stored
-__declspec(dllexport) bool GetStart(int& xpos, int& ypos)
+extern "C" __declspec(dllexport) bool GetStart(int& xpos, int& ypos)
 {
     if (sXPos >= 0 && sXPos <= mWidth && sYPos >= 0 && sYPos <= mHeight)
     {
@@ -112,7 +112,7 @@ __declspec(dllexport) bool GetStart(int& xpos, int& ypos)
 }
 
 // Sets ending location for player
-__declspec(dllexport) bool SetEnd(int xpos, int ypos)
+extern "C" __declspec(dllexport) bool SetEnd(int xpos, int ypos)
 {
     if (xpos >= 0 && xpos <= mWidth && ypos >= 0 && ypos <= mHeight) {
         eXPos = xpos;
@@ -125,7 +125,7 @@ __declspec(dllexport) bool SetEnd(int xpos, int ypos)
 
 // Get ending location for player
 // Return x and y, -1 if not stored 
-__declspec(dllexport) bool GetEnd(int& xpos, int& ypos)
+extern "C" __declspec(dllexport) bool GetEnd(int& xpos, int& ypos)
 {
     if (eXPos >= 0 && eXPos <= mWidth && eYPos >= 0 && eYPos <= mHeight)
     {
@@ -137,7 +137,7 @@ __declspec(dllexport) bool GetEnd(int& xpos, int& ypos)
     return false;
 }
 
-__declspec(dllexport) bool Restart() {
+extern "C" __declspec(dllexport) bool Restart() {
     currentIndex = 0;
     return true;
 }
